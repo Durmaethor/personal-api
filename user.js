42cb89b3e6d8d33e55191dc8e73f5549964a726e
Ryan Kramer <rkramer613@gmail.com>
2017-03-09 19:39:15 -0700

    Added in user info

--- user.js
+++ user.js
@@ -1,3 +1,50 @@
-const user = {};
+const user = {
+    name: 'Ryan Kramer',
+    location: 'Pleasant Grove',
+    occupations: ['HELL'],
+    hobbies: [
+        {
+        name: 'Video Games',
+        type: 'digital'
+        },
+        {
+        name: "Off-Roading",
+        type: 'outdoors'
+        },
+        {
+        name: 'Investing',
+        type: 'lucrative'
+        }
+    ],
+    family: [
+        {
+        name: 'Cassie Kramer',
+        relation: 'wife',
+        gender: 'female'
+        },
+        {
+        name: 'EmmaLeigh Kramer',
+        relation: 'child',
+        gender: 'female'
+        }
+    ],
+    restaurants: [
+        {
+        name: 'Taco Bell',
+        type: 'Fast Food Mexican',
+        rating: 5
+        },
+        {
+        name: 'JCWs',
+        type: 'Burgers',
+        rating: 5
+        },
+        {
+        name: 'Olive Garden',
+        type: 'Italian',
+        rating: 5
+        }
+    ]
+};
 
 module.exports = user;
